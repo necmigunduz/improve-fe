bd516377fad018eea74ed3c497fe95a679a16dde
Necmi Gunduz <necmigunduz@gmail.com>
2025-10-30 11:31:23 +0300

    add: count setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
-import ProfileCard from './components/ProfileCards/profileCard.jsx'
 import ProfileCards from './pages/ProfileCards/ProfileCards.jsx'
 
 function App() {
-
+  const [count, setCount] = useState(0)
+  
+  const increaseCount = () => {
+    setCount(count + 1)
+  }
+  const decreaseCount = () => {
+    setCount(count - 1)
+  }
 
   return (
     <>
+      <p>Count: {count} </p>
+      <button onClick={increaseCount}>Increase Count</button>
+      <button onClick={() => setCount(0)}>Reset Count</button>
+      <button onClick={decreaseCount}>Decrease Count</button>
       <ProfileCards /> 
     </>
   )
