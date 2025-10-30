0fff965803891a216a5600b750725b152fe647ad
Necmi Gunduz <necmigunduz@gmail.com>
2025-10-30 11:25:09 +0300

    add: image to profile card

--- src/components/ProfileCards/ProfileCard.jsx
+++ src/components/ProfileCards/ProfileCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './style.css'
 
-function Welcome({ firstName, lastName }) {
+function Welcome({ firstName, lastName, image }) {
   return (
     <div className="profile-card">
+      {image ? <><img src={image} alt={`${firstName} ${lastName}`} className="profile-image" height={100} width={100} /><hr /></> : <p>No Image Available</p>}
       <div><strong>First Name:</strong> {firstName} </div>
       <div><strong>Last Name:</strong> {lastName}</div>
     </div>
--- src/pages/ProfileCards/ProfileCards.jsx
+++ src/pages/ProfileCards/ProfileCards.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import ProfileCard from "../../components/ProfileCards/profileCard.jsx";
+import image1 from '../../assets/necmi.jpg';
 
 function ProfileCards() {
     const list = [
-        { firstName: "John", lastName: "Doe" },
+        { firstName: "John", lastName: "Doe", image: image1 },
         { firstName: "Jane", lastName: "Smith" },
         { firstName: "Alice", lastName: "Johnson" },
         { firstName: "Bob", lastName: "Brown" },
         {firstName: "Charlie", lastName: "Davis"}
     ];
     return list.map((person, index) => (
-        <>
+        <div key={index}>
             <h2>Team Members</h2>
             <p>Person {index + 1}</p>
-            <ProfileCard firstName={person.firstName} lastName={person.lastName} />
-        </>
+            <ProfileCard firstName={person.firstName} lastName={person.lastName} image={person.image}/>
+        </div>
     ));
 }
 
